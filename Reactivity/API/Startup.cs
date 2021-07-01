1f2e63602104280de573047079ce481fc1ea57a2
hampus <hampus.john.olausson@gmail.com>
2021-07-01 11:06:06 +0200

    Created DataContext and using Entity to connect

--- Reactivity/API/Startup.cs
+++ Reactivity/API/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using Persistence;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,17 +12,18 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration config;
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
-        }
+            _config = config;
 
-        public IConfiguration Configuration { get; }
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -31,6 +33,10 @@ namespace API
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DotNETAPI", Version = "v1" });
+            });
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
         }
 
